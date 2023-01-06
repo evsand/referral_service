1f123fea21fca66f906fd337d9defff541af9a31
evsand <andrevstyugin@gmail.com>
2023-01-06 19:47:49 +0300

    feat: async send mail

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,60 +1,27 @@
-from flask import current_app, url_for
+from flask import current_app, url_for, render_template
 from flask_mail import Message
 from app import mail
-from threading import Thread
+from app.email import send_email
 
 
 def send_confirm_email(user):
     token = user.generate_confirmation_token()
-    msg = Message('Подтверждение регистрации',
-                  sender='Referral Service',
-                  recipients=[user.email])
-    msg.body = f"""
-    Чтобы подтвердить Ваш аккаунт, перейдите по этой ссылке:
-    {url_for('auth.confirm_email', token=token, _external=True)}
-
-    Если вы не делали данный запрос, просто проигнорируйте это письмо!
-    Никаких изменений произведено не будет!
-
-    Отвечать на данное письмо не нужно так как оно сгенерировано автоматически.
-    """
-    mail.send(msg)
+    send_email('[RS] Подтверждение регистрации',
+               sender=current_app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/confirm_email.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/confirm_email.html',
+                                         user=user, token=token))
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    msg = Message('Запрос на смену пароля',
-                  sender='referralservice.helper@gmail.com',
-                  recipients=[user.email])
-    msg.body = f"""
-    Чтобы сбросить ваш пароль, перейдите по этой ссылке:
-    {url_for('auth.reset_password', token=token, _external=True)}
+    send_email('[RS] Запрос на смену пароля',
+               sender=current_app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
 
-    Если вы не делали данный запрос, просто проигнорируйте это письмо!
-    Никаких изменений произведено не будет!
-
-    Отвечать на данное письмо не нужно так как оно сгенерировано автоматически.
-    """
-    mail.send(msg)
-
-
-'''
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
-def send_email(subject, sender, recipients, text_body, html_body,
-               attachments=None, sync=False):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    if attachments:
-        for attachment in attachments:
-            msg.attach(*attachment)
-    if sync:
-        mail.send(msg)
-    else:
-        Thread(target=send_async_email,
-            args=(current_app._get_current_object(), msg)).start()
-'''
